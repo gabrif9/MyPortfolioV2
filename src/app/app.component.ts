4cb171fe67c5ac6f81476e0716225ed192b85529
g.ferrigno <g.ferrigno@studenti.unipi.it>
2025-04-13 21:40:36 +0200

    feat: add header, about and technologies sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { AboutComponent } from "./components/about/about.component";
+import { TechnologiesComponent } from "./components/technologies/technologies.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutComponent, TechnologiesComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
