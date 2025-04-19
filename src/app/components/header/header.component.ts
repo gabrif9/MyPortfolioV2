78415d29ce929160980c390a0607c3732ff7e284
g.ferrigno <g.ferrigno@studenti.unipi.it>
2025-04-19 18:07:36 +0200

    feat: add hamburger menu icon and logic to open the sidebar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [MenuComponent],
+  imports: [MenuComponent, SidebarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  showSidebarSignal = signal(false)
+
+  toggleSidebar(event: string){
+    this.showSidebarSignal.update(() => !this.showSidebarSignal())
+    if(event != '') {
+      console.log(document.getElementById(event))
+      document.getElementById(event)?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      })
+    }
+  }
 
 }
